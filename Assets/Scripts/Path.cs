c3df85d45299005157cdfcf3bbf94592be940b8a
Sir Flynn <flynnvanstryp@gmai.com>
2021-08-14 18:27:30 +1000

    3 Levels complete!

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -49,6 +49,7 @@ public class Path : MonoBehaviour
     {
         if (!ValidIndex(i)) return false;
 
+        Debug.Log(Vector3.Distance(pos, path[i]));
         if (Vector3.Distance(pos, path[i]) <= 0.9f)
         {
             return true;
